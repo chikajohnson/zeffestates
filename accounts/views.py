450eca330d29e6dbcc172768ecbb28d05c924ac8
johnson <johnson.iyida@netopng.com>
2020-06-14 17:43:29 +0100

    complete account module

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,67 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+
 
 def login(request):
-    return render(request, 'accounts/login.html', context={})
+    if request.method == 'POST':
+        # Get form variables
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are loged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'invalid username or password.')
+            return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+
 
 def register(request):
-    return render(request, 'accounts/register.html', context={})
+    if request.method == 'POST':
+        # Get form variables
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            # check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already exists.')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, email=email,
+                    first_name=first_name, last_name=last_name)
+                    # Login after regsiter.
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged in');
+                    user.save()
+                    messages.success(request, 'You are now regsitered and can log in');
+                    return redirect('login')
+        else:
+            messages.error(request, 'Password do not match')
+            return redirect('register')
+    else:
+        return render(request, 'accounts/register.html', context={})
+
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index')
 
 
-def dashbaord(request):
+def dashboard(request):
     return render(request, 'accounts/dashboard.html', context={})
